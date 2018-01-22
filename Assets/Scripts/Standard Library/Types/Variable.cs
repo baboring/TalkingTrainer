725b35ecc6c4299980fc56a48e3580f0df7c6dcf
Benjamin Park <baboring@gmail.com>
2018-01-21 23:08:09 -0500

    Updated Logger ( fixed bug for saving file in osx ) Added ConfigManager to handle variable type

---
+++ Assets/Scripts/Standard Library/Types/Variable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common {
+	public interface IVariable
+	{
+		System.Type Type {get;}
+		object Value {get;}
+	}
+
+	public class Variable : IVariable
+	{
+		public System.Type Type {get; private set;}
+		public object Value { get; set; }
+
+		public Variable(System.Type type, object value) {
+			Type = type;
+			Value = value;
+		}
+
+		public T GetValue<T>() {
+			if(typeof(T) == this.Type)
+				return (T)Value;
+			return default(T);
+		}
+
+		public bool SetValue<T>(T val) {
+			if(typeof(T) != this.Type)
+				return false;
+			Value = val;
+			return true;
+		}
+	}		
+
+}
