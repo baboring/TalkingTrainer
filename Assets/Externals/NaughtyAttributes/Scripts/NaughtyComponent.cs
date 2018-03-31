af4878fb68507843de44fd358f50e91abf640eaa
Benjamin Park <baboring@gmail.com>
2018-03-31 19:38:05 -0400

    Added Plugin NaughtAttributes (https://github.com/baboring/NaughtyAttributes)

---
+++ Assets/Externals/NaughtyAttributes/Scripts/NaughtyComponent.cs
@@ -0,0 +1,46 @@
+using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaughtyComponent : MonoBehaviour
+{
+    public bool isInteger = true;
+
+    [ShowIf("isInteger")]
+    [Header("integer")]
+    public int integer;
+
+    [Header("sprite")]
+    [ShowAssetPreview]
+    public Sprite sprite;
+
+    [Header("zero")]
+    [ReadOnly]
+    public Vector3 zero = Vector3.zero;
+
+    private int[] values = new int[] { 1, 2, 3, 4 };
+
+    [Header("dropdown")]
+    [Dropdown("values")]
+    public int dropdown;
+
+    [Header("minMaxSlider")]
+    [MinMaxSlider(0, 1)]
+    public Vector2 minMaxSlider;
+
+    [Header("slider")]
+    [Slider(0, 10)]
+    public int slider;
+
+    [Header("health")]
+    [ProgressBar]
+    public float health = 50;
+
+    [Header("list")]
+    [ReorderableList]
+    public float[] list;
+
+    [Header("textArea")]
+    [ResizableTextArea]
+    public string textArea;
+}
